41f921ae579f9f7b62283ea9bbab468bfbfb77cd
Jovan Zoric <jzjovanzoric@gmail.com>
2020-12-15 21:18:56 +0100

    Zadatak 5

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,6 +3,9 @@
 @section('title', $movie->title)
 
 @section('content')
-<h1>{{$movie->title}}</h1>
-<p>{{$movie->zaplet}}</p>
+    <h1>{{$movie->title}}</h1>
+    <h6>Genre: {{$movie->genre}}</h6>
+    <h6>Director: {{$movie->director}}</h6>
+    <h6>Release Year: {{$movie->release_year}}</h6>
+    <p>Zaplet: {{$movie->zaplet}}</p>
 @endsection
